bab910cd320a24317458c0be731a22a3a5964f82
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-02-27 23:30:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+const mapToNegativize = function(array) {
+    let newArray = [] 
+        for(const el of array) {
+            newArray.push(el * -1)
+        }
+    return newArray
+}; 
+
+const mapToNoChange = function(array) {
+    return array 
+}; 
+
+const mapToDouble = function(array) {
+    let newArray = []
+        for(const el of array) {
+            newArray.push(el *2)
+        }
+    return newArray
+}; 
+
+const mapToSquare = function(array) {
+    let newArray = [] 
+        for(const el of array) {
+            newArray.push(el ** 2)
+        }
+    return newArray
+}; 
+
+const reduceToTotal = function(array, startingPoint = 0) {
+    let total = startingPoint
+        for(const el of array) {
+            total += el
+        }
+    return total 
+}; 
+
+const reduceToAllTrue = function(array){
+    for(const el of array) {
+        if(!el) {return false}
+    }
+    return true 
+}; 
+
+const reduceToAnyTrue = function(array) {
+    for(const el of array) {
+        if (!!el) {return true}
+    }
+    return false 
+}
