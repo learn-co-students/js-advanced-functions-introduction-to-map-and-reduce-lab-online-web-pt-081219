7feb58aa7e042c6ffcb21dbfd0300fa549819dfb
umberto palazzo <upalazzo93@gmail.com>
2020-03-29 13:39:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Your code here
+
+function negativize(num) {
+    return num * -1
+}
+
+function mapToNegativize(array){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(negativize(array[i]))
+    }
+
+    return newArray
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(array[i] * 2)
+    }
+
+    return newArray
+}
+
+function mapToSquare(array){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(array[i] ** 2)
+    }
+
+    return newArray
+}
+
+function reduceToTotal(array, startingPoint = 0){
+    let total = startingPoint
+    for(let i = 0; i < array.length; i++){
+        total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array){
+    let result 
+    for(let i = 0; i < array.length; i++){
+        if (!!array[i]){
+            result = true
+        } else {
+            result = false 
+            break
+        }
+    }
+    return result
+}
+
+function reduceToAnyTrue(array){
+    let result 
+    for(let i = 0; i < array.length; i++){
+        if (!!array[i]){
+            result = true
+            break
+        } else {
+            result = false
+        }
+    }
+    return result
+}
+
+
