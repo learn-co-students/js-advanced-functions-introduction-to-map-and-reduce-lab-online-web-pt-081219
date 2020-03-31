c11c47ab9befa7df4d7017dcbbecc65c7abfe5a8
Jack <jackrrye@gmail.com>
2020-03-30 19:55:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,82 @@
-// Your code here
+// Map Methods
+// Remember, all map methods return a new Array
+
+// mapToNegativize(sourceArray)
+function mapToNegativize(arr) {
+    const newArr = [];
+    let n = 0;
+    arr.forEach(element => {
+        n = element * -1;
+        newArr.push(n);
+    });
+    return newArr
+}
+
+// mapToNoChange(sourceArray)
+function mapToNoChange(arr) {
+    return arr;
+}
+
+// mapToDouble(sourceArray)
+function mapToDouble(arr) {
+    const newArr = [];
+    let n = 0;
+    arr.forEach(element => {
+        n = element * 2;
+        newArr.push(n);
+    });
+    return newArr
+}
+
+// mapToSquare(sourceArray)
+function mapToSquare(arr) {
+    const newArr = [];
+    let n = 0;
+    arr.forEach(element => {
+        n = element * element;
+        newArr.push(n);
+    });
+    return newArr
+}
+
+// Reduce Methods
+// Remember, all reduce methods return a value
+
+// reduceToTotal(sourceArray, startingPoint)
+function reduceToTotal(arr, x) {
+    if (x === undefined) {
+        let n = 0;
+        arr.forEach(element => {
+            n += element;
+        })
+        return n;
+    }
+    else {
+        arr.forEach(element => {
+            x += element;
+        })
+        return x;
+    }
+}
+
+// reduceToAllTrue(sourceArray)
+function reduceToAllTrue(arr) {
+    let x = true
+    arr.forEach(element => {
+        if(element === false) {
+            x = false;
+        }
+    })
+    return x;
+}
+
+// reduceToAnyTrue(sourceArray)
+function reduceToAnyTrue(arr) {
+    let x = false;
+    arr.forEach(element => {
+        if(element === true) {
+            x = true;
+        }
+    })
+    return x;
+}
