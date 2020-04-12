cbd67de4862a428df318e2f778c16e5a6fff0863
Maria Le <hp.mars.le@gmail.com>
2020-04-11 22:40:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+    let map = []
+    for (let i = 0; i < sourceArray.length; i++){
+        map.push(-sourceArray[i]);
+        
+    }
+    return map; 
+    // return sourceArray.map((num) => -num)
+}
+
+function mapToNoChange(sourceArray) {
+    let map = []
+    for (let i = 0; i < sourceArray.length; i++){
+        map.push(sourceArray[i]);
+        
+    }
+    return map; 
+    // return sourceArray.map((num) => num)
+}
+
+function mapToDouble(sourceArray) {
+    let map = []
+    for (let i = 0; i < sourceArray.length; i++){
+        map.push(sourceArray[i]*2);
+        
+    }
+    return map; 
+    // return sourceArray.map((num) => num * 2)
+}
+
+function mapToSquare(sourceArray) {
+    let map = []
+    for (let i = 0; i < sourceArray.length; i++){
+        map.push(sourceArray[i]**2);
+        
+    }
+    return map; 
+    // return sourceArray.map((num) => num**2)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+
+    let reduce = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+        reduce += sourceArray[i];
+    }
+    return reduce;
+
+}
+
+function reduceToAllTrue(sourceArray) {
+
+    let reduce = true
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) {
+        return reduce = false
+        }
+    }
+    return reduce
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let reduce = false;
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {
+            return true
+        }
+    }
+    return reduce  
+
+}
